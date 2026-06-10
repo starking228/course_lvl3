3bf00bf303af3a2eb39db3e86f34866c41e1705b
Starking <starking3737z@gmail.com>
2026-06-10 13:31:21 +0300

    Add AMQ consumer, autoreformat code of all classes

--- src/main/java/com/chychula/ActiveMqConsumer.java
+++ src/main/java/com/chychula/ActiveMqConsumer.java
@@ -1,51 +1,56 @@
 package com.chychula;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.jms.*;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 
+import javax.jms.*;
 import java.util.Properties;
 
-public class ActiveMqProducer implements AutoCloseable {
+public class ActiveMqConsumer implements AutoCloseable {
 
     private final Connection connection;
     private final Session session;
-    private final MessageProducer producer;
-    //    private final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+    private final MessageConsumer consumer;
+    private final ObjectMapper mapper = new ObjectMapper()
+            .registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
     private final Properties properties = PropertiesUtil.getLoadedProperties("config.properties");
     private final String QUEUE_NAME = properties.getProperty("QueueName", "defaultName");
     private final String BROKER_URL = properties.getProperty("BrokerURL", "tcp://localhost:61616");
 
 
-    private final ObjectMapper mapper = new ObjectMapper()
-            .registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
-
-    public ActiveMqProducer() throws JMSException {
+    public ActiveMqConsumer() throws JMSException {
         ConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
         this.connection = factory.createConnection();
         this.connection.start();
 
         this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination destination = session.createQueue(QUEUE_NAME);
-        this.producer = session.createProducer(destination);
-        this.producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+        this.consumer = session.createConsumer(destination);
     }
 
-    public void send(Message message) {
+    public Message receive() throws JMSException {
+        javax.jms.Message jmsMsg = consumer.receive(2000);
+
+        if (jmsMsg == null) {
+            return null;
+        }
+
+        if (!(jmsMsg instanceof TextMessage)) {
+            throw new RuntimeException("Unsupported JMS message type: " + jmsMsg);
+        }
+
+        String json;
         try {
-            String json = mapper.writeValueAsString(message);
-            TextMessage textMessage = session.createTextMessage(json);
-            producer.send(textMessage);
+            json = ((TextMessage) jmsMsg).getText();
+            return mapper.readValue(json, Message.class);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to send message", e);
+            throw new RuntimeException("Failed to deserialize message", e);
         }
     }
 
     @Override
-    public void close() throws JMSException {
-        producer.close();
+    public void close() throws Exception {
+        consumer.close();
         session.close();
         connection.close();
     }
